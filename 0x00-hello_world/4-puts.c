074c06a72430c25b10dc79e3f8ae58cd08197a64
seun <seun_leke@yahoo.com>
2023-02-16 08:40:34 -0800

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints a string
- * Description: Puts the string message to the screen
- * puts - Displays output to the screen
- * Return: Always 0 (succesd)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
