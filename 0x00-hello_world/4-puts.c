4af087450690228d8ba12c375563a78dbac1bcb9
seun <seun_leke@yahoo.com>
2023-02-16 08:29:39 -0800

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Prints a string
+ * Description: Puts the string message to the screen
+ * puts - Displays output to the screen
+ * Return: Always 0 (succesd)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return(0);
 }
