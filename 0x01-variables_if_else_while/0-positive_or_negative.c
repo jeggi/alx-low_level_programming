e49df1aa8da5e53200df044540281296ba4cbfa3
seun <seun_leke@yahoo.com>
2023-02-17 06:41:45 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+
 /**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
- * Return: Always 0 (Success)
+ * Return: Always 0 (Seun == Success)
  */
-/* betty style doc for function main goes there */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 int main(void)
 {
 int n;
