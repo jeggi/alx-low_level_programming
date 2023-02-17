dfc6f00314316e903401b8336ce552375987b922
seun <seun_leke@yahoo.com>
2023-02-17 06:31:39 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
- * Return: Always 0 (Seun == Success)
+ * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
 int main(void)
