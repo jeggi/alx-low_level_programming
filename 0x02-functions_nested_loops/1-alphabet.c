2561567fb01097605272719d92c15287d4109b50
seun <seun_leke@yahoo.com>
2023-02-21 05:22:14 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - is to print alphabet
+ *
+ * Return: 0;
+ *
+ */
+void print_alphabet(void)
+	{
+		char myLetter = 'a';
+
+		while (myLetter <= 'z')
+		{
+			_putchar(myletter);
+			myletter++;
+
+		}
+		_putchar('\n);
+}
+
