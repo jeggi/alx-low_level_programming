22017e07892ad52218f81a7f3cc0a2be719e82fd
seun <seun_leke@yahoo.com>
2023-02-21 05:53:05 -0800

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - mo fe print alphabet le me wa - Yoruba
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int circle = 0;
+	char myLetter = 'a';
+
+	while (circle < 10)
+	{
+		myLetter = 'a'
+		while (myLetter <= 'z')
+		{
+			_putchar(myLetter);
+			myLetter++;
+		}
+		_putchar('\n');
+
+		circle++;
+	}
+}
+
