04d74209697f4c1f27904d15f79c96e25b0510b0
seun <seun_leke@yahoo.com>
2023-02-21 05:09:44 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
-* main - prints _putchar
-*
-* Description: prints _putchar
-*
-* Return: 0 (success)
-*/
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-int s = 'a';
-while (s <= 'z')
-{
-_putchar(s);
-s++
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
-_putchar("\n");
-}
+
