c08c1440874b108ac314f33611379c228d24cb71
seun <seun_leke@yahoo.com>
2023-02-21 04:34:17 -0800

    the first assingment

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - this is for function
+* description: alphabets printing
+* Return: 0;
+*/
+int main(void)
+{
+int s = 'a';
+while (s <= 'z')
+{
+putchar(s);
+s++
+}
+putchar("\n");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This reame file is for 0x02-functions_nested_loops
