93669316aaf63d1273c4d09260d35ee7c0eec5e6
seun <seun_leke@yahoo.com>
2023-02-23 17:20:05 -0800

    advance

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,7 +19,7 @@ void putformat(int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
+void print_number(void);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
