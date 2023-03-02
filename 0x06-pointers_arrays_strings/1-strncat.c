57e4dde7bfa2fd275587c838bf15f1945097df37
seun <seun_leke@yahoo.com>
2023-03-02 03:59:00 -0800

    mandatory

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,33 @@
-nclude "main.h"
+#include "main.h"
+
 /**
- *  * _strncat - concate
- *   * @dest: great
- *    * @src: source
- *     * @n: bytes
- *      *
- *       * Return: dest
+ * _strncat - two words
+ * @dest : pointer to char param
+ * @src : pointer to char param
+ * @n : int parameter
+ * Return: *dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *pr = dest;
+	int m;
+	int i;
 
-	while (*pr != '\0')
+	m = 0;
+
+	for (i = 0; i < 1000; i++)
 	{
-		pr++;
-	}
-	while (n--)
-	{
-		while (!(*pr++ = *src++))
+		if (dest[i] == '\0')
 		{
-			return (dest);
+			break;
 		}
+		m++;
 	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
 	return (dest);
 }
