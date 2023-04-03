97283222dc678ae68eccb9fc8f13bfa2033ed391
jeggi <seun_leke@yahoo.com>
2023-04-03 18:31:24 +0100

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+*print_list - function that prints all the element of a list
+*@h: variable pointing to the head
+*Return: the number of the nodes
+*/
+
+size_t print_listit(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+
+return (nnodes);
+
+}		
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is the readme file for this directory 0x13-more_singly_linked_lists
