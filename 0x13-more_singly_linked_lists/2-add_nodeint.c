f2bae8315bd222e307f7df6be5fbbe9af1e5177f
jeggi <seun_leke@yahoo.com>
2023-04-04 11:06:38 +0100

    fixing betty style in question 1

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - adding a new node in the beginning of a linked list
+ * @head: head of a list
+ * @n: declaring a constant element n
+ * Return: stop at NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *add = malloc(sizeof(listint_t));
+	
+	if(add == NULL){
+	       return (NULL);
+			}
+	add->n = n;
+	add->next = *head;
+	*head = add;
+
+return (*head);
+}
