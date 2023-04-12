35cf528d884c482fbb53797e482b9bbc2f5dd707
jeggi <seun_leke@yahoo.com>
2023-04-13 00:59:48 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
  * @file_to: where the file would be to
  * Return: this is a journey of no return
  */
- void error_file(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -21,42 +21,43 @@
 }
 
 /**
- * 
- *
+ * main - cheking this code
+ *@argc: this is the arguement
+ *@argv: arguments
+ *Return: 0
  */
- int main(int argc, char *argv[])
- {
-	 char buf[1024];
-	 ssize_t nwchars, wr;
-	 int file_from, file_to, mis_close;
-	 
-	 if (argc !=3)
-	 {
-		 dprintf(STDERR_FILENO, "%s\n", "Use - copy file_from file_to");
-		 exit(97);
-	 }
-	 
-	 file_from = open(argv[1], O_RDONLY);
-	 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	 error_file(file_from, file_to, argv);
-	 
-	 nwchars = 1024;
-	 while (nwchars == 1024)
-	 {
-		 nwchars = read(file_from, buf, 1024);
-		 if (nwchars == -1)
-			 error_file(-1, 0, argv);
-		 wr = write(file_to, buf, nwchars);
-		 if (wr == -1)
-			 error_file(0, -1, argv);
-	 }
-	 
-	 mis_close = close(file_from);
-	 if (mis_close == -1)
-	 {
-		 dprintf(STDERR_FILENO, "Error: this doc %d\n can't close", file_from);
-		 exit(100);
-	 }
-	 return (0);
- }
- 
+int main(int argc, char *argv[])
+{
+	char buf[1024];
+	ssize_t nwchars, wr;
+	int file_from, file_to, mis_close;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Use - copy file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nwchars = 1024;
+	while (nwchars == 1024)
+	{
+		nwchars = read(file_from, buf, 1024);
+		if (nwchars == -1)
+			error_file(-1, 0, argv);
+
+		wr = write(file_to, buf, nwchars);
+		if (wr == -1)
+			error_file(0, -1, argv);
+	}
+
+	mis_close = close(file_from);
+	if (mis_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: this doc %d\n can't close", file_from);
+		exit(100);
+	}
+	return (0);
+}
