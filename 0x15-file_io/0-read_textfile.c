c74869c429fa947b7b064a58986ff0fb055e25ca
jeggi <seun_leke@yahoo.com>
2023-04-13 17:34:10 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,36 @@
 #include "main.h"
+
 /**
-* read_textfile - this function that reads a text file.
-* @filename: the text file.
-* @letters: every letters.
-* Return: actual number of letters 0 if it fails.
-*/
+ *read_textfile - a function that reads a text file.
+ *@filename: the text file
+ *@letters: every letters
+ *
+ *Return: actual number of letters
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor, r, w;
+	int doc, s, w;
 	char buff[BUFSIZ];
 
 	if (filename == NULL || letters == 0)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
+	doc = open(filename, O_RDONLY);
+	if (doc == -1)
 		return (0);
 
-	r = read(file_descriptor, buff, letters);
+	s = read(doc, buff, letters);
 	if (r == -1)
 		return (0);
 
-	w = write(STDOUT_FILENO, buff, r);
+	w = write(STDOUT_FILENO, buff, s);
 	if (w == -1)
 		return (0);
 
-	if (r != w)
+	if (s != w)
 		return (0);
 
-	close(file_descriptor);
+	close(doc);
 
 	return (w);
 }
