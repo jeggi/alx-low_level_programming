a312e4a8c5ef12c14cab3d32a0019f8a0961d45a
jeggi <seun_leke@yahoo.com>
2023-04-13 17:22:28 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * error_file - checks if files can open using dprintf
- * @doc_from: where the file is from
+ * @file_from: where the file is from
  * @argv: the arguement
- * @doc_to: where the file would be to
+ * @file_to: where the file would be to
  * Return: this is a journey of no return
  */
-void error_file(int doc_from, int doc_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (doc_from == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (doc_to == -1)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -28,42 +28,42 @@ void error_file(int doc_from, int doc_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int doc_from, doc_to, err_close;
-	ssize_t nchars, nwr;
+	int file_from, file_to, err_close;
+	ssize_t nwchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp doc_from doc_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	doc_from = open(argv[1], O_RDONLY);
-	doc_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(doc_from, doc_to, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	nwchars = 1024;
+	while (nwchars == 1024)
 	{
-		nchars = read(doc_from, buf, 1024);
-		if (nchars == -1)
+		nwchars = read(file_from, buf, 1024);
+		if (nwchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(doc_to, buf, nchars);
+		nwr = write(file_to, buf, nwchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
-	err_close = close(doc_from);
+	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", doc_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(doc_to);
+	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", doc_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
