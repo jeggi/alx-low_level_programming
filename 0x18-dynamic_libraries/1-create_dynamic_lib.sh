cadabe40725f310593cbe7c788b64b9d79ccfdef
jeggi <seun_leke@yahoo.com>
2023-06-23 18:01:12 +0100

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o libdynamic.so
-gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -ldynamic -o len
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
+rm *.o
