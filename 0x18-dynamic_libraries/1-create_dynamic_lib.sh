23b5921617afdfbadbe67bc3298b83cdb987d80c
jeggi <seun_leke@yahoo.com>
2023-07-11 23:25:48 +0100

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+# Compile all .c files in the current directory into object files
+gcc -c -fPIC *.c
+
+# Create the dynamic library using the object files
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
 rm *.o
+
 echo "Dynamic library liball.so created successfully."
